769facd0a53865d112e24c603de7ef25dfeaf437
AntoineLaLune <Antoine.24h@outlook.fr>
2026-05-26 16:06:23 +0200

    Edit and add Fat JAR, and remove port from url

--- src/main/java/fr/bts/iris/slam/model/UrlEnum.java
+++ src/main/java/fr/bts/iris/slam/model/UrlEnum.java
@@ -2,12 +2,12 @@ package fr.bts.iris.slam.model;
 
 public enum UrlEnum {
 
-    LOGIN("http://framelab.xn--trebois-owa.ip-ddns.com:3333/api/auth/login"),
-    REGISTER("http://framelab.xn--trebois-owa.ip-ddns.com:3333/api/auth/register"), // IDEE
-    SESSION("http://framelab.xn--trebois-owa.ip-ddns.com:3333/api/auth/session"),
-    CHALLENGE("http://framelab.xn--trebois-owa.ip-ddns.com:3333/api/challenges/current"),
-    UPLOAD("http://framelab.xn--trebois-owa.ip-ddns.com:3333/uploads"),
-    SUBMIT("http://framelab.xn--trebois-owa.ip-ddns.com:3333/api/participations");
+    LOGIN("http://framelab.xn--trebois-owa.ip-ddns.com/api/auth/login"),
+    REGISTER("http://framelab.xn--trebois-owa.ip-ddns.com/api/auth/register"), // IDEE
+    SESSION("http://framelab.xn--trebois-owa.ip-ddns.com/api/auth/session"),
+    CHALLENGE("http://framelab.xn--trebois-owa.ip-ddns.com/api/challenges/current"),
+    UPLOAD("http://framelab.xn--trebois-owa.ip-ddns.com/uploads"),
+    SUBMIT("http://framelab.xn--trebois-owa.ip-ddns.com/api/participations");
 
     private final String url;
 
